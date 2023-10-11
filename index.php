87244fd12125a43107f8c71ba24c1411fb088bfe
Joao Moraes <joao.moraes@ba.senac.br>
2023-10-11 19:28:43 -0300

    Atualização 01

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <script src="https://code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="main.js"></script>
+    <title>JAVASCRIPT</title>
+  </head>
+  <body>
+    <h2>Calcular</h2>
+
+	
+        <label for="valor01">valor01:</label>
+		<input type="text" name="valor01" id="valor01"><br><br>
+		
+		<label for="valor02">valor02:</label>
+		<input type="text" name="valor02" id="valor02"><br><br>
+		
+		<button type="calcular" id="calcular"  Onclick = "Calcule()">Calcule</button>
+
+        <p id="resposta"></p>
+        <br><br>
+  
+  </body>
+</html>
